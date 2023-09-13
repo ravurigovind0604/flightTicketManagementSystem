a10c251bdd46676985aa493ec105d9e8b372e0d1
Ravuri govind <67058717+ravurigovind0604@users.noreply.github.com>
2023-09-13 12:37:08 -0500

    Add files via upload

---
+++ Gautamexplain/src/main/java/com/example/Gautamexplain/GautamexplainApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gautamexplain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GautamexplainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GautamexplainApplication.class, args);
+	}
+
+}
---
+++ Gautamexplain/src/test/java/com/example/Gautamexplain/GautamexplainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gautamexplain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GautamexplainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
